fb49f39fea43641903878198fcc8c2633d06a0b4
Mike Monaco <mamonaco1973@gmail.com>
2025-08-22 07:10:34 -0400

    create SSM activation code

---
+++ 01-directory/activation.tf
@@ -0,0 +1,47 @@
+# ------------------------------------------------------------------------------
+# Create IAM Role for Hybrid Instances
+# ------------------------------------------------------------------------------
+
+resource "aws_iam_role" "ssm_hybrid_instance_role" {
+  name = "ssm-hybrid-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = {
+        Service = "ssm.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance" {
+  role       = aws_iam_role.ssm_hybrid_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# ------------------------------------------------------------------------------
+# Create SSM Hybrid Activation
+# ------------------------------------------------------------------------------
+
+resource "aws_ssm_activation" "hybrid_activation" {
+  name               = "HybridActivation"
+  description        = "Activation for registering on-prem or non-EC2 machines"
+  iam_role           = aws_iam_role.ssm_hybrid_instance_role.name
+  registration_limit = 10   # Number of servers you can register with this activation
+  expiration_date    = timeadd(timestamp(), "720h") # optional, 30 days from now
+}
+
+# ------------------------------------------------------------------------------
+# Outputs
+# ------------------------------------------------------------------------------
+
+output "activation_id" {
+  value = aws_ssm_activation.hybrid_activation.activation_id
+}
+
+output "activation_code" {
+  value = aws_ssm_activation.hybrid_activation.activation_code
+}
--- 01-directory/mini-ad.tf
+++ 01-directory/mini-ad.tf
@@ -46,6 +46,8 @@ resource "aws_instance" "mini_ad_dc_instance" {
     ADMINISTRATOR_PASS = random_password.admin_password.result
     ADMIN_USER_PASS    = random_password.admin_password.result
     USERS_JSON         = local.users_json
+    ACTIVATION_CODE    = aws_ssm_activation.hybrid_activation.activation_code
+    activation_id      = aws_ssm_activation.hybrid_activation.activation_id
   })
 
   tags = {
