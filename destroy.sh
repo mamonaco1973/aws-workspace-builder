ce40ad9f8a714cdf415b4b8a9bfef76093286b68
mamonaco1973 <mamonaco1973@gmail.com>
2025-08-22 18:42:30 +0000

    fix destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -76,4 +76,3 @@ cd .. || exit
 # Completion
 # --------------------------------------------------------------------------------------------------
 echo "NOTE: Infrastructure destruction complete."
-git
